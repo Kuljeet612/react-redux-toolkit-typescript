d849282115f911e264f0b2741652003726c3d264
Kuljeet Kaur <kuljeet.kaur@oracle.com>
2022-05-31 17:06:40 +0530

    deleting unused file

--- src/redux/testUseAppSelector.ts
+++
@@ -1,9 +0,0 @@
-const state = {
-    mood: {
-        cart: '',
-        products: ''
-    }
-}
-
-//export const testUseAppSelector = (f:any) => f(state);
-export const testUseAppSelector = jest.fn(() => state);
